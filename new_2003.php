13e7c0a73ef23f30f4814dedcd556a5372583b5e
Grace Qiu <gravitation@gmail.com>
2013-03-05 19:39:56 -0600

    Made "Lite" the new home page

--- new_2003.php
+++
@@ -1,6 +0,0 @@
-<?php
-# Dodo says use this script to test! #
-include("header.inc");
-include("new_2003.inc");
-include("footer.inc");
-?>
--- updates.php
+++
@@ -1,6 +0,0 @@
-<?php
-# Dodo says use this script to test! #
-include("header.inc");
-include("updates.inc");
-include("footer.inc");
-?>
